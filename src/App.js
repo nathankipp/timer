b92859f5d078fbcea9583cd7d4f1f7c5ba9d5643
Nathan Kipp <nkipp@signal.co>
2019-09-20 16:33:40 -0500

    working timer w/ session storage backup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const SESSION_STORAGE_KEY = 'nsk-timer';
+const TWO_MINUTES = 1000 * 60 * 70;
+
+const fmt = d => d < 10 ? `0${d}` : d;
+
+const App = () => {
+  const [time, setTime] = useState(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || TWO_MINUTES);
+  let timer = useRef();
+
+  useEffect(() => {
+    timer.current = setInterval(() => setTime(t => t-1000), 1000);
+  }, []);
+
+  useEffect(() => {
+    if (time <= 0) {
+      window.sessionStorage.removeItem(SESSION_STORAGE_KEY);
+      clearInterval(timer.current);
+    } else {
+      window.sessionStorage.setItem(SESSION_STORAGE_KEY, time);
+    }
+  }, [time]);
+
+  const min = Math.trunc(time/60/1000);
+  const sec = (time/1000)-(min*60);
+
+  return <>{fmt(min)}:{fmt(sec)}</>;
+};
 
 export default App;
